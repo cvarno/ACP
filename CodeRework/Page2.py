ba24a110b49330abe6273c743f2db5b6fd8f8d85
Cayden Varno <cayden.varno@gmail.com>
2022-10-05 16:46:40 -0400

    rename and add pointers to main.py

--- CodeRework/Page2.py
+++ CodeRework/Page2.py
@@ -4,3 +4,6 @@ class Page2(tk.Frame): #Initial Page Frame
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self['bg'] = '#375280'
+
+if __name__ == '__main__':
+    exec(open("CodeRework/main.py").read())
